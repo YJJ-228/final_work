0beea6cd68c467fe8eba330f5464c704c00b77ca
yjj <326465833@qq.com>
2023-05-19 17:09:29 +0800

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Data_analyzing/draw_functions.py
    	modified:   Download/.gitignore
    	modified:   Download/window_1.py
    	modified:   Download/window_2.py

--- Download/window_2.py
+++ Download/window_2.py
@@ -2,16 +2,16 @@ import ttkbootstrap as ttk
 import downloader as dl
 
 def download():
-    url=entry1.get()
+    url=entry2.get()
     dl.large_documents(url=url)
 
 root = ttk.Window()
 root.geometry("400x400")
 root.title("窗口示例")
-lb1=ttk.Label(root,text="请输入链接")
-entry1 =ttk.Entry(root, width=40)
-bt1=ttk.Button(root,text="下载",width=30,command=download)
-lb1.pack()
-entry1.pack()
-bt1.pack(pady=50)
+lb2=ttk.Label(root,text="请输入链接")
+entry2 =ttk.Entry(root, width=40)
+bt2=ttk.Button(root,text="下载",width=30,command=download)
+lb2.pack()
+entry2.pack(pady=30)
+bt2.pack(pady=20)
 root.mainloop()
