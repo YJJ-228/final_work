a5c8a125c794ef1a8f67da022529ccd8b81fb946
yjj <326465833@qq.com>
2023-05-24 09:12:30 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Webcrawler/stock.py
    
    Changes not staged for commit:
    	modified:   Webcrawler/file.csv

--- Webcrawler/stock.py
+++ Webcrawler/stock.py
@@ -3,9 +3,9 @@ import json
 import schedule
 
 #沪A股单日排行
-# url='https://push2.eastmoney.com/api/qt/clist/get?fid=f62&po=1&pz=50&pn=1&np=1&fltt=2&invt=2&fs=m:1+t:2+f:!2,m:1+t:23+f:!2&fields=f12,f14,f2,f3,f62,f184,f66,f69,f72,f75,f78,f81,f84,f87,f204,f205,f124,f1,f13'
+url='https://push2.eastmoney.com/api/qt/clist/get?fid=f62&po=1&pz=50&pn=1&np=1&fltt=2&invt=2&fs=m:1+t:2+f:!2,m:1+t:23+f:!2&fields=f12,f14,f2,f3,f62,f184,f66,f69,f72,f75,f78,f81,f84,f87,f204,f205,f124,f1,f13'
 #沪A股三日排行
-url='https://push2.eastmoney.com/api/qt/clist/get?fid=f267&po=1&pz=50&pn=1&np=1&fltt=2&invt=2&fs=m:1+t:2+f:!2,m:1+t:23+f:!2&fields=f12,f14,f2,f127,f267,f268,f269,f270,f271,f272,f273,f274,f275,f276,f257,f258,f124,f1,f13'
+# url='https://push2.eastmoney.com/api/qt/clist/get?fid=f267&po=1&pz=50&pn=1&np=1&fltt=2&invt=2&fs=m:1+t:2+f:!2,m:1+t:23+f:!2&fields=f12,f14,f2,f127,f267,f268,f269,f270,f271,f272,f273,f274,f275,f276,f257,f258,f124,f1,f13'
 header={
     'Referer':'http://quote.eastmoney.com/',
     'User-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50',
@@ -24,14 +24,17 @@ def stock_taking():
         response = requests.get(url, headers=header, cookies=cookie, params=param)
         if response.status_code == 200:
             j = json.loads(response.text)
-            
             for item in j['data']['diff']:
                 name = item['f14']
                 data = item['f12']
-                increase='+%'+str(item['f3'])
+                increase='%'+str(item['f3'])
                 new_price=item['f2']
                 table.append([data,name,increase,new_price])
                 print('{:<11}{:<6}{:<11}{}'.format(data,name,increase,new_price))
+                with open('D:/Code/final_work/Webcrawler/file.csv','w',encoding='utf-8')as f:
+                    for row in table:
+                        line = ','.join([str(elem) for elem in row]) + '\n'
+                        f.write(line)
     return table
 
 def job():
